09b925d2e27a9b33e841997fbecaa68741522f0f
satyabratait <satyabrata@itobuz.com>
2023-03-29 18:54:33 +0530

    third commit

--- modules/functions.mjs
+++ modules/functions.mjs
@@ -1,24 +1,39 @@
 import { cardInfo } from "./database/cardInformation.mjs";
 import { aboutUs } from "./database/aboutUs.mjs";
 import { roomData } from "./database/slider.mjs";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export let topData = () => {
+const fileName = fileURLToPath(import.meta.url);
+
+const dirName = path.dirname(fileName);
+let filePath = "./database/roomData.txt";
+
+export function topData() {
   return JSON.stringify({
     message: "success",
     data: aboutUs,
   });
-};
+}
 
-export let cardData = () => {
+export function cardData() {
   return JSON.stringify({
     message: "success",
     data: cardInfo,
   });
-};
+}
 
-export let roomsData = () => {
+export function roomsData() {
   return JSON.stringify({
     message: "success",
     data: roomData,
   });
-};
+}
+
+export async function addRoomData(data) {
+  let readFileData = await fs.readFile(path.resolve(dirName, filePath),"utf-8");
+  readFileData = JSON.parse(readFileData);
+  readFileData.push(data);
+  await fs.writeFile(path.resolve(dirName, filePath), JSON.stringify(readFileData));
+}
--- server.mjs
+++ server.mjs
@@ -1,27 +1,46 @@
 import http from "http";
+import  url  from "url";
+import { parse } from "querystring";
 import * as data from "./modules/functions.mjs";
+import { roomData } from "./modules/database/slider.mjs";
+
+
+function routes(req){
+  const parseUrl = url.parse(req.url, true);
+  const path = parseUrl.pathname;
+  if (path === "/top") {
+    console.log(data.topData);
+    return data.topData();
+  }
+ else if (path === "/card") {
+    console.log(data.cardData());
+    return data.cardData();
+  } 
+  else if (path === "/slider"){
+    console.log(data.roomsData());
+    return data.roomsData();
+  }
+}
 
 http.createServer(function (req, res) {
   try {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Header", "*");
-    if (req.url === "/top") {
-      res.end(data.topData());
-    }
-   else if (req.url === "/card") {
-      res.end(data.cardData());
-    } 
-    else if (req.url==="/slider"){
-      res.end(data.roomsData())
-    }
-    else {
-      res.end(
-        JSON.stringify({
-          success: false,
-          message: "Data not Found",
-        })
-      );
-    }
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.writeHead(200, {"Content-Type" : "application/json"});
+    let chunks = "";
+    req.on("data",(chunk) => {
+      chunks = chunk.toString();
+    });
+
+    req.on("end",()=>{
+      let parsedData = parse(chunks);
+      console.log(parsedData);
+      if (Object.keys(parsedData).length !== 0) {
+        data.addRoomData(parsedData);
+      }
+    });
+    
+    res.end(routes(req));
   } catch (err) {
     console.log(err);
   }
